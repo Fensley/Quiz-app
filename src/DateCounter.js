bd3b291671eaae9fd1f6f3c568238a97df7a0a59
Fensley <fortilusnehemie@gmail.com>
2024-06-15 14:19:27 -0400

    initial commit

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -4,6 +4,7 @@ function DateCounter() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
 
+  console.log(step);
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
